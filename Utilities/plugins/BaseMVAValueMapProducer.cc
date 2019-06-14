bab794fd4709e7053d62e548f4a07f828065c5e8
Cristina <criss.ms7@gmail.com>
2019-06-14 12:41:46 -0500

    point to the correct the mvautils

--- Utilities/plugins/BaseMVAValueMapProducer.cc
+++ Utilities/plugins/BaseMVAValueMapProducer.cc
@@ -37,7 +37,7 @@
 
 #include "CommonTools/Utils/interface/StringObjectFunction.h"
 #include "DataFormats/Common/interface/ValueMap.h"
-#include "CommonTools/MVAUtils/interface/TMVAZipReader.h"
+#include "CommonTools/Utils/interfaceTMVAZipReader.h"
 #include "DataFormats/PatCandidates/interface/Jet.h"
 #include "DataFormats/PatCandidates/interface/Muon.h"
 #include "DataFormats/PatCandidates/interface/Electron.h"
