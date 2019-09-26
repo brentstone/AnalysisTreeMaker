43e785856e78ee765b2d21756bfb909301bf2da4
Nick McColl <nickmccoll@gmail.com>
2019-09-25 17:42:14 -0700

    METFilters work in all three years

--- TreeMaker/run/searchRegionTreeMaker_cfg.py
+++ TreeMaker/run/searchRegionTreeMaker_cfg.py
@@ -94,7 +94,7 @@ process.treeMaker = cms.EDAnalyzer('SearchRegionTreeMaker'
                                  , sample = cms.string(sample)
                                  , type = cms.string(type)
                                  , EventFiller               = cms.PSet(EventFiller)
-#                                  , METFilterFiller           = cms.PSet(METFilterFiller)
+                                 , METFilterFiller           = cms.PSet(METFilterFiller)
 #                                  , TriggerFiller             = cms.PSet(TriggerFiller)
 #                                  , ak4JetFiller              = cms.PSet(ak4JetFiller)
 #                                  , ak4PuppiJetFiller         = cms.PSet(ak4PuppiJetFiller)
@@ -167,6 +167,31 @@ if '2017' in type or '2016' in type : #https://twiki.cern.ch/twiki/bin/viewauth/
     if not isRealData :
         process.treeMaker.EventFiller.addPrefiringWeights = True
         process.p += process.prefiringweight
+if '2017' in type or '2018' in type : #https://twiki.cern.ch/twiki/bin/viewauth/CMS/MissingETOptionalFiltersRun2
+    process.load('RecoMET.METFilters.ecalBadCalibFilter_cfi')
+    baddetEcallist = cms.vuint32(
+        [872439604,872422825,872420274,872423218,
+         872423215,872416066,872435036,872439336,
+         872420273,872436907,872420147,872439731,
+         872436657,872420397,872439732,872439339,
+         872439603,872422436,872439861,872437051,
+         872437052,872420649,872422436,872421950,
+         872437185,872422564,872421566,872421695,
+         872421955,872421567,872437184,872421951,
+         872421694,872437056,872437057,872437313])
+    
+    
+    process.ecalBadCalibReducedMINIAODFilter = cms.EDFilter(
+        "EcalBadCalibFilter",
+        EcalRecHitSource = cms.InputTag("reducedEgamma:reducedEERecHits"),
+        ecalMinEt        = cms.double(50.),
+        baddetEcal    = baddetEcallist, 
+        taggingMode = cms.bool(True),
+        debug = cms.bool(False)
+        )    
+    process.p += process.ecalBadCalibReducedMINIAODFilter
+    process.treeMaker.METFilterFiller.ecalBadCalibFilterUpdate = True;    
+    
  
 # #==============================================================================================================================#
 #==============================================================================================================================#
